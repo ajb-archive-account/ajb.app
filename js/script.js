26c6dc2bce876e691835789d8e320b5b2c0d9b22
andybyers21 <andy.byers21@gmail.com>
2020-07-30 16:55:32 +0100

    added portfolio and minor styling changes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Menu Scrolling Effect
+
+$(window).on("scroll", function () {
+  if ($(window).scrollTop()) {
+    $("nav").addClass("black");
+  } else {
+    $("nav").removeClass("black");
+  }
+});
