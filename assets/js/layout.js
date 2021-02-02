12bf0c03945e3b381f02c5754c2326b8da4d0d32
andybyers21 <andy.byers21@gmail.com>
2021-02-02 14:48:03 +0000

    build home page cards

---
+++ assets/js/layout.js
@@ -0,0 +1,14 @@
+// Apply CSS changes on `fresh` page reload 
+
+(function() {
+  let h, a, f;
+  a = document.getElementsByTagName('link');
+  for (h = 0; h < a.length; h++) {
+    f = a[h];
+    if (f.rel.toLowerCase().match(/stylesheet/) && f.href) {
+      let g = f.href.replace(/(&|\?)rnd=\d+/, '');
+      f.href = g + (g.match(/\?/) ? '&' : '?');
+      f.href += 'rnd=' + (new Date().valueOf());
+    }
+  } // for
+})()
